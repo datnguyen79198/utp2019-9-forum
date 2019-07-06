c87df6a76eb62db374a6f004473a62e8aff77b9b
unknown <datnguyen79198@gmail.com>
2019-07-06 18:16:58 +0300

    #8 push a thread to database

--- database/Threads/Threads.js
+++ database/Threads/Threads.js
@@ -2,6 +2,17 @@ var fs = require('fs');
 
 var dbPath = __dirname + '/threads.json';
 
+class Thread {
+    constructor(id,author,title,entry,tags,date) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.entry = entry;
+        this.tags = tags;
+        this.date = date;
+    }
+}
+
 exports.connect = () => {
     // Create new json file if don't exist
     try {
@@ -15,4 +26,30 @@ exports.connect = () => {
             fs.writeFileSync(dbPath, JSON.stringify(threads, '', 4), 'utf-8');
         } else console.log(err);
     }
+}
+
+exports.createThread = (author,title,entry,tags) => {
+    return new Promise((resolve,reject) => {
+        fs.readFile(dbPath,'utf-8',(err,db) => {
+            if (err) {
+                console.log('Error when request to Users database');
+                reject(err);
+            } else {
+                db = JSON.parse(db);
+
+                var thread = new Thread(Math.random().toString(36).slice(2),author,title,entry,tags,new Date().getTime());
+                db.Threads.push(thread);
+
+                db_to_json = JSON.stringify(db,'',4);
+                fs.writeFile(dbPath, db_to_json, 'utf-8', (err) => {
+                    if (err) {
+                        console.log('Error while writing to db');
+                        reject(err);
+                    } else {
+                        resolve(thread);
+                    }
+                })
+            }
+        });
+    });
 }
--- routers/thread.js
+++ routers/thread.js
@@ -4,6 +4,21 @@ var sendMethod = require('./sendResponse');
 
 exports.createThread = (req,res) => {
     utils.checkBody(req,res).then(result => {
-        console.log('done check post thread Signal'+ result.title + ' ' + result.entry + ' ' + result.tags);
+        var cookie = utils.checkCookie(req);
+        console.log('done check post thread Signal'+ result.title + ' ' + result.entry + ' ' + result.tags + ' ' + cookie['username']);
+        db.threads.createThread(cookie['username'],result.title,result.entry,result.tags).then(result => {
+            console.log('done create thread to db');
+            sendMethod.sendResponse('./views/main.html',res,'text/html',cookie,'/');
+        })
+        .catch(err => {
+            console.log(err);
+            res.statusCode = 400;
+            res.end('Something go wrong');
+        });
+    },
+    err => {
+      console.log(err);
+      res.statusCode = 400;
+      res.end('Something go wrong');
     });
 }
