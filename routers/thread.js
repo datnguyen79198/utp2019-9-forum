330c19abc7571cb1addd3fe3e1550c660f0779e1
unknown <datnguyen79198@gmail.com>
2019-07-06 17:43:10 +0300

    #8 server get info by POST method from front

--- routers/thread.js
+++ routers/thread.js
@@ -3,5 +3,7 @@ var db = require('../database/mainDatabase');
 var sendMethod = require('./sendResponse');
 
 exports.createThread = (req,res) => {
-    
+    utils.checkBody(req,res).then(result => {
+        console.log('done check post thread Signal'+ result.title + ' ' + result.entry + ' ' + result.tags);
+    });
 }
