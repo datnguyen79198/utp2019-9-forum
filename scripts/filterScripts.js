40dc47d37236699ef8545772ac62377b777d6970
unknown <datnguyen79198@gmail.com>
2019-07-17 11:36:01 +0300

    #14 display all thread found

--- scripts/filterScripts.js
+++ scripts/filterScripts.js
@@ -34,17 +34,72 @@ xmlHttp.onreadystatechange = function() {
         for (var i=0;i<Object.keys(myDB.Threads).length;i++) numberOfThread+=(myDB.Threads[i].tags==filterTag);
         console.log(numberOfThread);
         if (numberOfThread==0) {
-            findRes = document.createElement('tr');
+            var findRes = document.createElement('tr');
             var inner = document.createElement('th');
             inner.innerHTML = '<font size="3"><i>'+"Sorry, there were no community results for " + filterTag + " tag"+'</i></font>';
             findRes.appendChild(inner);
             document.getElementById("displayThread").appendChild(findRes);
         } else {
-            findRes = document.createElement('tr');
+            var findRes = document.createElement('tr');
             var inner = document.createElement('th');
+            inner.setAttribute('width','100%');
             inner.innerHTML = '<font size="3"><i>'+numberOfThread + " results found with " + filterTag + " tag"+'</i></font>';
             findRes.appendChild(inner);
             document.getElementById("displayThread").appendChild(findRes);
+
+            var tab = document.createElement('table');
+            var title = document.createElement('tr');
+            title.setAttribute("style","background-color: #2D90EC; color: white");
+            tab.setAttribute("width","100%");
+            title.innerHTML = '<th width = "10%">Tag</th>'
+                             +'<th width = "60%">Thread title</th>'
+                             +'<th class="text-center" width = "10%">Upvote </th>'
+                             +'<th class="text-center" width = "10%">Comments </th>'
+                             +'<th witdh = "10%">Author </th>';
+            tab.appendChild(title);
+
+            var thread;
+            for (var i=0;i<Object.keys(myDB.Threads).length;i++) {
+                if (myDB.Threads[i].tags!=filterTag) continue;
+                thread = document.createElement('tr');
+                var tags = document.createElement('th');
+                tags.setAttribute('width',"10%");
+                tags.innerHTML = myDB.Threads[i].tags;
+                thread.appendChild(tags);
+
+                var title = document.createElement('th');
+                title.setAttribute('width',"60%");
+                title.setAttribute('style',"background-color : #E5EEFD");
+
+                var mainTitle = document.createElement('a');
+                mainTitle.setAttribute('href','/thread/'+myDB.Threads[i].id);
+                mainTitle.innerHTML = myDB.Threads[i].title;
+                title.appendChild(mainTitle);
+                thread.appendChild(title);
+
+                var vote = document.createElement('th');
+                vote.setAttribute('class',"text-center");
+                vote.setAttribute('width',"10%");
+                vote.innerHTML = myDB.Threads[i].upVote;
+                thread.appendChild(vote);
+
+                var cmt = document.createElement('th');
+                cmt.setAttribute('width',"10%");
+                cmt.setAttribute('style',"background-color : #E5EEFD");
+                cmt.setAttribute('class',"text-center");
+                cmt.innerHTML = Object.keys(myDB.Threads[i].comments).length;
+                thread.appendChild(cmt);
+
+                var author = document.createElement('th');
+                author.setAttribute('width',"10%");
+                author.innerHTML = myDB.Threads[i].author;
+                thread.appendChild(author);
+                thread.setAttribute('height','30');
+                tab.appendChild(thread);
+            }
+
+            document.getElementById("displayThread").appendChild(tab);
+
         }
     }
 };
